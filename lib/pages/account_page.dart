c1a3fd09db37c93874fd8397b8bab373e551f7aa
tyasenj <tyasenj@gmail.com>
2024-10-25 08:33:54 +0700

    add: profile

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -5,10 +5,207 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'),
+        title: Text(''),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
-        child: Text('Account Screen'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.blueGrey,
+              padding: EdgeInsets.only(bottom: 50.0, left: 30.0, right: 30.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage('assets/images/profile.png'),
+                  ),
+                  SizedBox(width: 30), // distance between image and text
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Walter White',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'walterwhite69@gmail.com',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Notification and Dark Mode
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(1.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.notifications),
+                    title: Text(
+                      'Notification',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Switch(
+                      value: true, // Set initial value here
+                      onChanged: (value) {
+                        // Handle switch toggle
+                      },
+                      activeColor: Colors.white,
+                      activeTrackColor: Colors.black,
+                      inactiveThumbColor: Colors.black,
+                      inactiveTrackColor: Colors.white,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.dark_mode),
+                    title: Text(
+                      'Dark Mode',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Switch(
+                      value: false, // Set initial value here
+                      onChanged: (value) {
+                        // Handle switch toggle
+                      },
+                      activeColor: Colors.white,
+                      activeTrackColor: Colors.black,
+                      inactiveThumbColor: Colors.black,
+                      inactiveTrackColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Change Profile, Change Password, & About the App
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(1.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text(
+                      'Change Profile',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios, size: 20),
+                    onTap: () {
+                      // Navigate to change profile page
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.lock),
+                    title: Text(
+                      'Change Password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios, size: 20),
+                    onTap: () {
+                      // Navigate to change password page
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text(
+                      'About the App',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios, size: 20),
+                    onTap: () {
+                      // Navigate to about app page
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Log Out
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle log out
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.logout, color: Colors.white),
+                        SizedBox(width: 10),
+                        Text(
+                          'Log Out',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Icon(Icons.arrow_forward_ios,
+                        color: Colors.white, size: 20),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
