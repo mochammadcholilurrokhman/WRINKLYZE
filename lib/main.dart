34e8d0de6f820f4a365cff783eeaaea4b56920fa
Fyodor-DOs <fathir123t@gmail.com>
2024-11-06 12:37:48 +0700

    add: installation firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,14 @@ import 'package:wrinklyze_6/pages/login.dart';
 import 'package:wrinklyze_6/pages/onboarding.dart';
 import 'package:wrinklyze_6/pages/register_page.dart';
 import 'package:wrinklyze_6/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
